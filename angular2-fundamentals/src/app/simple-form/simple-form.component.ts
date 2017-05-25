fbf2a3b544bcfc9d4a0b290fe7d6b38d118d9d69
Jack Matthews <jackhkmatthews@gmail.com>
2017-05-25 11:42:33 +0100

    initial commit

---
+++ angular2-fundamentals/src/app/simple-form/simple-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-form',
+  template: `
+    <p>
+      hardform-pigs Works!
+    </p>
+  `,
+  styles: []
+})
+export class SimpleFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
